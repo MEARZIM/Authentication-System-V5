9d26ce39a0502c9ed2a3f6d1444f670c8103ab82
MEARZIM <asahaayan@gmail.com>
2024-03-10 00:43:04 +0530

    Handle callbacks

--- auth.ts
+++ auth.ts
@@ -2,7 +2,10 @@ import NextAuth from "next-auth"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 
 import { db } from "@/lib/db"
+import { getUserById } from "@/data/user"
 import authConfig from "@/auth.config"
+import { UserRole } from "@prisma/client"
+
 
 export const {
   handlers: { GET, POST },
@@ -10,7 +13,43 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
+  callbacks: {
+    // async signIn ({user}) {
+    //   if (!user) return false;
+
+    //   const existUser = await getUserById(user.id as string);
+
+    //   if (!existUser || !existUser.emailVerified) return false;
+
+    //   console.log(user)
+    //   return true
+    // },
+
+    async session({ token, session },) {
+      console.log({ token })
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+
+      if (token.role && session.user) {
+        session.user.role = token.role as UserRole;
+      }
+      return session;
+    },
+
+    async jwt({ token }) {
+      if (!token.sub) return token;
+
+      const existUser = await getUserById(token.sub);
+
+      if (!existUser) return token;
+
+      token.role = existUser.role;
+
+      return token
+    }
+  },
   adapter: PrismaAdapter(db),
-  session: { strategy : "jwt"}, // Prisma session strategy database dosent work
+  session: { strategy: "jwt" }, // Prisma session strategy database dosent work
   ...authConfig,
 })
